5ee4148da5332b9d85f6e69fae3c102565069967
pnovoselov <Novosselov.pavel@gmail.com>
2022-11-04 00:18:13 +0300

    added spring boot starter

---
+++ src/test/java/ru/pnm/dbmanger/DbmangerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pnm.dbmanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class DbmangerApplicationTests {
+
+//	@Test
+	void contextLoads() {
+	}
+
+}
